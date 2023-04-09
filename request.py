bb152c87a023cdf683e9fab44b000eb76f7e8ab6
kazumaendo <kazuma@afyaglobal.com>
2023-04-09 16:04:45 -0700

    Analyze token activity

--- request.py
+++ request.py
@@ -3,7 +3,9 @@ from google.auth.transport.requests import AuthorizedSession
 from datetime import datetime,timedelta
 import json
 import time
+import operator
 
+# Part 1
 timestr = time.strftime("%Y%m%d-%H%M%S")
 file_name = timestr + ".json"
 # 48 hours ago timestamp
@@ -41,3 +43,42 @@ with open(file_name,'w') as f:
     for i in items:
         json.dump(i,f)
         f.write('\n')
+
+# Part 2
+user_events = dict()
+method_bytes = dict()
+params = list()
+# read 48 hours of data from Part 1, newline-delimited JSON file
+with open(file_name) as f:
+    for line in f:
+        j_content = json.loads(line)
+        # grab user information
+        user = j_content["actor"]["profileId"]
+        # grab api method and number of bytes to applications
+        events = j_content["events"]
+        api_method = ""
+        api_bytes = 0
+        for e in events:
+            for sub_e in e["parameters"]:
+                # method name for an event instance
+                if sub_e["name"] == "method_name":
+                    api_method = sub_e["value"]
+                # number of bytes for an event instance
+                elif sub_e["name"] == "num_response_bytes":
+                    api_bytes = int(sub_e["intValue"])
+        # update number of event associated with user
+        user_events[user] = user_events.get(user,0) + 1
+        # update number of bytes an api method uses. sum
+        method_bytes[api_method] = method_bytes.get(api_method,0) + api_bytes
+
+max_user = max(user_events.items(),key=operator.itemgetter(1))[0]
+max_api_method = max(method_bytes.items(),key=operator.itemgetter(1))[0]
+sorted_user_events = dict(sorted(user_events.items(), key=operator.itemgetter(1),reverse=True))
+sorted_method_bytes = dict(sorted(method_bytes.items(), key=operator.itemgetter(1),reverse=True))
+print(f'User with most number of events (ID): {max_user}')
+print(f'API method with most number of bytes: {max_api_method}')
+print('----------------------------------------------------------')
+print('Complete list of events per user:')
+print(sorted_user_events)
+print('Complete list of number of bytes per API method:')
+print(sorted_method_bytes)
